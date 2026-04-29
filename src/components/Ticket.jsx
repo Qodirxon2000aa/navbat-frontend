071771b494dde0779cbd4a62d6b36e951b159c85
Qodirxon2000a <namesuz41@gmail.com>
2026-04-30 00:14:08 +0500

    Dfd

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -50,7 +50,7 @@ export default function Ticket({
   // Bitta chek komponenti
   const TicketCard = () => (
     <div
-      style={{ fontFamily: "'Courier New', Courier, monospace", width: "70mm" }}
+      style={{ fontFamily: "'Courier New', Courier, monospace", width: "75mm" }}
       className="bg-white text-black rounded overflow-hidden shadow-xl"
     >
       {/* Yuqori yirtish chizig'i */}
